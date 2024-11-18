a6ed5f80b2accb0893595e919afd3a90afb73f4e
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2024-11-18 11:34:36 -0800

    catalog product and initial entities

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,9 @@
+namespace Shared.DDD;
+
+public interface IAggregate<TId> : IAggregate, IEntity<TId> { }
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
