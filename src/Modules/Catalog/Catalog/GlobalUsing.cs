0f368900223058de5ec1e025e9b12fc3d1916230
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2024-11-19 10:41:05 -0800

    CQRS generic types

--- src/Modules/Catalog/Catalog/GlobalUsing.cs
+++ src/Modules/Catalog/Catalog/GlobalUsing.cs
@@ -2,6 +2,7 @@ global using System.Reflection;
 global using Catalog.Data;
 global using Catalog.Products.Events;
 global using Catalog.Products.Models;
+global using MediatR;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Shared.Data;
